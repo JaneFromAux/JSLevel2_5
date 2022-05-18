f51eb8fa39216c17c8fb15387308e5d8bcd2f3b8
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-18 19:39:03 +0200

    space deleted

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,16 +25,16 @@ function check() {
         case "Hessen":
             Ergebnis.innerHTML = "Hessen hat 6,176 Mio Einwohner und Wiesbaden ist die Hauptstadt";
             break;
-        case "Mecklenburg - Vorpommern":
+        case "Mecklenburg-Vorpommern":
             Ergebnis.innerHTML = "Mecklenburg-Vorpommern hat 1,612 Mio Einwohner und Schwerin ist die Hauptstadt";
             break;
         case "Niedersachsen":
             Ergebnis.innerHTML = "Niedersachsen hat 7,927 Mio Einwohner und Hannover ist die Hauptstadt";
             break;
-        case "Nordrhein - Westfalen":
+        case "Nordrhein-Westfalen":
             Ergebnis.innerHTML = "Nordrhein-Westfalen hat 17,865 Mio Einwohner und Düsseldorf ist die Hauptstadt";
             break;
-        case "Rheinland - Pfalz":
+        case "Rheinland-Pfalz":
             Ergebnis.innerHTML = "Rheinland-Pfalz hat 4,053 Mio Einwohner und Mainz ist die Hauptstadt";
         case "Saarland":
             Ergebnis.innerHTML = "Saarland hat 0,996 Mio Einwohner und Saarbrücken ist die Hauptstadt";
@@ -42,10 +42,10 @@ function check() {
         case "Sachsen":
             Ergebnis.innerHTML = "Sachsen hat 4,085 Mio Einwohner und Dresden ist die Hauptstadt";
             break;
-        case "Sachsen - Anhalt":
+        case "Sachsen-Anhalt":
             Ergebnis.innerHTML = "Sachsen-Anhalt hat 2,245 Mio Einwohner und Magdeburg ist die Hauptstadt";;
             break;
-        case "Schleswig - Holstein":
+        case "Schleswig-Holstein":
             Ergebnis.innerHTML = "Schleswig-Holstein hat 2,859 Mio Einwohner und Kiel ist die Hauptstadt";
             break;
         case "Thürigen":
